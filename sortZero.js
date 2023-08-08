c7e78ae06207c58213554ca46e0eca48b4ada37a
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-08-08 20:08:20 +0530

    sortZero code

---
+++ sortZero.js
@@ -0,0 +1,17 @@
+const arr = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0];
+
+function sortZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] == 1 && arr[j] == 0) {
+        //Swap
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+}
+
+sortZero(arr);
+console.log(arr);
