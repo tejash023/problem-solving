8c0a5899c5a532426ded8222a1648773e1c9a55d
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-08-08 21:06:52 +0530

    optimized code

---
+++ sortZeroOptimized.js
@@ -0,0 +1,17 @@
+function sortZeroOptimized(arr) {
+  let index = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      // Swap 0 and 1
+      let temp = arr[i];
+      arr[i] = arr[index];
+      arr[index] = temp;
+
+      index++;
+    }
+  }
+}
+
+sortZeroOptimized(arr);
+console.log(arr);
