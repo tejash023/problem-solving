8dc5482839c3b7a2e5fdec7de945a4abde489148
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-09-05 18:46:16 +0530

    added code for rotate image

---
+++ 14.rotateImage.js
@@ -0,0 +1,28 @@
+let rotateImageV2 = (input) => {
+  const n = input.length - 1;
+  for (let i = 0; i <= n; i++) {
+    for (let j = i; j <= n; j++) {
+      const temp = input[i][j];
+      input[i][j] = input[j][i];
+      input[j][i] = temp;
+    }
+  }
+
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j <= n / 2; j++) {
+      const temp = input[i][j];
+      input[i][j] = input[i][n - j];
+      input[i][n - j] = temp;
+    }
+  }
+};
+
+let input = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+rotateImageV2(input);
+
+console.log(input);
